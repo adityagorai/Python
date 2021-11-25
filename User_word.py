b459e8bd34a9cbeef1666ecd9277713cbc84b6ba
Aditya Gorai <83246353+adityagorai@users.noreply.github.com>
2021-11-25 22:13:22 +0530

    Add files via upload

---
+++ User_word.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+word = 'Good Morning'
+user_word = input('Type the word "' + word + '":')
+while user_word != word:
+    user_word = input('Try again!: ')
+print('Correct!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
